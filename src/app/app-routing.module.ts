48bf54ce99ebe8f1031e11f9eb6ee94f286cd08d
Andrei-David Nan <nandavid190@gmail.com>
2023-12-11 10:16:46 +0200

    v0.0.5 - set up routing module, bootstrap styles and script;

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./component/login/login.component";
+import {RegisterComponent} from "./component/register/register.component";
+import {ResetPassComponent} from "./component/reset-pass/reset-pass.component";
+import {VerifyComponent} from "./component/verify/verify.component";
+import {ProfileComponent} from "./component/profile/profile.component";
+import {HomeComponent} from "./component/home/home.component";
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'reset-password', component: ResetPassComponent },
+  { path: 'user/verify/account/:key', component: VerifyComponent },
+  { path: 'user/verify/password/:key', component: VerifyComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
