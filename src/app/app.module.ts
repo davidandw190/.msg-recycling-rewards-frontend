758897e819a591913e1324dd652d6245c015d721
Andrei-David Nan <nandavid190@gmail.com>
2023-12-11 10:06:00 +0200

    v0.0.2 - set up initial components, interfaces, installed bootstrap;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { VerifyComponent } from './component/verify/verify.component';
+import { ResetPassComponent } from './component/reset-pass/reset-pass.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VerifyComponent,
+    ResetPassComponent,
+    ProfileComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/reset-pass/reset-pass.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-pass',
+  templateUrl: './reset-pass.component.html',
+  styleUrls: ['./reset-pass.component.css']
+})
+export class ResetPassComponent {
+
+}
