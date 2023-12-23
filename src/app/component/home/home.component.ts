3368b58a38cb1aad869a5c8a02b7e923fd9db67f
Andrei-David Nan <nandavid190@gmail.com>
2023-12-23 21:07:44 +0200

    v0.3.4 - configure home component and template;

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {HomePageResponse} from "../../interface/home-page-response";
+import {CustomHttpResponse} from "../../interface/custom-http-response";
+import {DataState} from 'src/app/enum/data-state.enum';
+import {AppState} from "../../interface/app-state";
+import {BehaviorSubject, catchError, map, Observable, of, startWith, switchMap, tap} from "rxjs";
+import {Router} from "@angular/router";
+import {CenterService} from "../../service/center.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +13,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  homeState$: Observable<AppState<CustomHttpResponse<HomePageResponse>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<HomePageResponse>>(null);
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingSubject.asObservable();
+  private currentPageSubject = new BehaviorSubject<number>(0);
+  currentPage$ = this.currentPageSubject.asObservable();
+
+  readonly DataState = DataState;
+
+  constructor(
+    private router: Router,
+    private centerService: CenterService
+  ) {}
+
+  ngOnInit(): void {
+    this.homeState$ = this.currentPage$
+      .pipe(
+        switchMap(pageNumber => this.centerService.centers$(pageNumber)),
+        tap(response => {
+          console.log(response);
+          this.dataSubject.next(response);
+        }),
+
+        map(response => ({ dataState: DataState.LOADED, appData: response })),
+
+        startWith({ dataState: DataState.LOADING }),
+
+        catchError(error => of({ dataState: DataState.ERROR, error }))
+      );
+  }
 
 }
