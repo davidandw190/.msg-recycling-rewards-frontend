002e32eeffeb2f213a51c1d7dcb214bb8ace1b55
Andrei-David Nan <nandavid190@gmail.com>
2023-12-24 13:00:15 +0200

    v0.3.9 - set up centers component fields and behaviour;

--- src/app/component/center-details/center-details.component.ts
+++ src/app/component/center-details/center-details.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {DataState} from "../../enum/data-state.enum";
+import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
+import {AppState} from "../../interface/app-state";
+import {CustomHttpResponse} from "../../interface/custom-http-response";
+import {CentersPageResponse} from "../../interface/centers-page-response";
+import {Router} from "@angular/router";
+import {CenterService} from "../../service/center.service";
 
 @Component({
   selector: 'app-center-details',
@@ -6,5 +13,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./center-details.component.css']
 })
 export class CenterDetailsComponent {
+  centersState$: Observable<AppState<CustomHttpResponse<CentersPageResponse>>>
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<CentersPageResponse>>(null);
 
+  readonly DataState = DataState;
+
+  constructor(
+    private router: Router,
+    private customerService: CenterService) {}
+
+  ngOnInit(): void {
+    this.centersState$ = this.customerService.searchCenters$()
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next(response);
+          return { dataState: DataState.LOADED, appData: response };
+        }),
+
+        startWith({ dataState: DataState.LOADING }),
+
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR, error })
+        })
+      )
+  }
 }
