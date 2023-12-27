2cc674ddfce8c0486980c35aab11566296a779a9
Andrei-David Nan <nandavid190@gmail.com>
2023-12-27 18:55:45 +0200

    v0.4.4 - finally set up city and county control to be reactive;

--- src/app/component/center-new/center-new.component.ts
+++ src/app/component/center-new/center-new.component.ts
@@ -1,63 +1,145 @@
-import {Component, OnInit} from '@angular/core';
-import {CustomHttpResponse} from "../../interface/custom-http-response";
-import {AppState} from "../../interface/app-state";
-import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
-import {CentersPageResponse} from "../../interface/centers-page-response";
-import {CenterNewResponse} from "../../interface/center-new-response";
-import {DataState} from 'src/app/enum/data-state.enum';
-import {CenterService} from "../../service/center.service";
-import {NgForm} from "@angular/forms";
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, catchError, debounceTime, distinctUntilChanged, filter, map, Observable, of, startWith, switchMap, tap } from 'rxjs';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Import Validators
+import { TypeaheadMatch } from 'ngx-bootstrap/typeahead';
+import { CustomHttpResponse } from '../../interface/custom-http-response';
+import { AppState } from '../../interface/app-state';
+import { CentersPageResponse } from '../../interface/centers-page-response';
+import { DataState } from '../../enum/data-state.enum';
+import { CenterService } from '../../service/center.service';
+import { LocationService } from '../../service/location.service';
 
 @Component({
   selector: 'app-center-new',
   templateUrl: './center-new.component.html',
-  styleUrls: ['./center-new.component.css']
+  styleUrls: ['./center-new.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class CenterNewComponent implements OnInit {
   newCenterState$: Observable<AppState<CustomHttpResponse<CentersPageResponse>>>;
-  private dataSubject  = new BehaviorSubject<CustomHttpResponse<CenterNewResponse>>(null);
-  private isLoadingSubject: BehaviorSubject<Boolean> = new BehaviorSubject<boolean>(false);
-  isLoading$: Observable<Boolean> = this.isLoadingSubject.asObservable();
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<CentersPageResponse>>(null);
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
 
   readonly DataState = DataState;
 
-  constructor(private centerService: CenterService) {}
+  countySelectForm: FormGroup;
+  citySelectForm: FormGroup;
+
+  counties: string[] = this.locationService.getAllCounties();
+  cities: string[] = [];
+
+  constructor(
+    private router: Router,
+    private centerService: CenterService,
+    private locationService: LocationService,
+    private formBuilder: FormBuilder
+  ) {
+    this.countySelectForm = this.formBuilder.group({
+      county: ['', Validators.required], // Add Validators.required
+    });
+
+    this.citySelectForm = this.formBuilder.group({
+      city: { value: '', disabled: true },
+    });
+
+  }
 
   ngOnInit(): void {
-    this.newCenterState$ = this.centerService.centers$()
-      .pipe(
-        map(response => {
-          console.log(response);
-          this.dataSubject.next(response);
-          return { dataState: DataState.LOADED, appData: response };
-        }),
+    this.newCenterState$ = this.centerService.centers$().pipe(
+      map((response) => {
+        console.log(response);
+        this.dataSubject.next(response);
+        return { dataState: DataState.LOADED, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING }),
+      catchError((error: string) => of({ dataState: DataState.ERROR, error }))
+    );
 
-        startWith({ dataState: DataState.LOADING }),
+    this.countySelectForm
+      .get('county')
+      .valueChanges.pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+      tap(() => this.citySelectForm.get('city').setValue('')),
+      tap((county) => this.handleCountyChange(county)),
+      switchMap((value) => this.filterCounties(value)),
+      tap((counties) => (this.counties = counties))
+    )
+      .subscribe();
 
-        catchError((error: string) => {
-          return of({ dataState: DataState.ERROR, error: error })
-        })
-      )
+    this.citySelectForm
+      .get('city')
+      .valueChanges.pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+      switchMap((value) => this.filterCities(value)),
+      tap((cities) => (this.cities = cities)),
+      filter(() => false)
+    )
+      .subscribe();
   }
 
-  createCenter(newCustomerForm: NgForm) {
+  createCenter(): void {
     this.isLoadingSubject.next(true);
-    this.newCenterState$ = this.centerService.create$(newCustomerForm.value)
-      .pipe(
-        map(response => {
-          console.log(response);
-          newCustomerForm.reset({ type: 'INDIVIDUAL', status: 'ACTIVE' });
-          this.isLoadingSubject.next(false);
-          return { dataState: DataState.LOADED, appData: this.dataSubject.value };
-        }),
-
-        startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
-
-        catchError((error: string) => {
-          this.isLoadingSubject.next(false);
-          return of({ dataState: DataState.LOADED, error: error })
-        })
-      )
+    this.centerService.create$(this.countySelectForm.value).pipe(
+      map((response) => {
+        console.log(response);
+        this.countySelectForm.reset({ county: '', city: '' });
+        this.isLoadingSubject.next(false);
+        return { dataState: DataState.LOADED, appData: this.dataSubject.value };
+      }),
+      startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
+      catchError((error: string) => {
+        this.isLoadingSubject.next(false);
+        return of({ dataState: DataState.LOADED, error });
+      })
+    ).subscribe((result) => {
+      this.newCenterState$ = of(result);
+    });
   }
 
+  onSelectCounty(event: TypeaheadMatch): void {
+    const selectedCounty = event.item;
+    this.loadCitiesForCounty(selectedCounty);
+  }
+
+  filterCounties(value: string): Observable<string[]> {
+    const filterValue = value.toLowerCase();
+    return of(this.counties.filter((county) => county.toLowerCase().includes(filterValue)));
+  }
+
+  private loadCitiesForCounty(county: string): void {
+    this.cities = this.locationService.getCitiesForCounty(county);
+    this.citySelectForm.get('city').enable();
+  }
+
+  filterCities(value: string): Observable<string[]> {
+    const filterValue = value.toLowerCase();
+    const selectedCounty = this.countySelectForm.get('county').value;
+    const citiesForCounty = this.locationService.getCitiesForCounty(selectedCounty);
+
+    if (!filterValue) {
+      return of(citiesForCounty);
+    }
+
+    return of(citiesForCounty.filter((city) => city.toLowerCase().includes(filterValue)));
+  }
+
+  private handleCountyChange(county: string): void {
+    const cityControl = this.citySelectForm.get('city');
+
+    if (this.locationService.isValidCounty(county)) {
+      cityControl.enable();
+      this.loadCitiesForCounty(county);
+    } else {
+      cityControl.disable();
+      cityControl.setValue('');
+    }
+  }
+
+  onSelectCity(event: TypeaheadMatch): void {
+    // Handle city selection if needed
+  }
 }
