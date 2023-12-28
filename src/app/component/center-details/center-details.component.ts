b17980e3294aed01d4f8f7f752ec19f2ee383aad
Andrei-David Nan <nandavid190@gmail.com>
2023-12-28 17:29:11 +0200

    v0.4.9 - configure core center details component and tamplate;

--- src/app/component/center-details/center-details.component.ts
+++ src/app/component/center-details/center-details.component.ts
@@ -1,10 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, catchError, map, Observable, of, startWith, switchMap} from "rxjs";
+import {AppState} from "../../interface/app-state";
+import {CustomHttpResponse} from "../../interface/custom-http-response";
+import {DataState} from "../../enum/data-state.enum";
+import {CenterDetailsResponse} from "../../interface/center-details-response";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {CenterService} from "../../service/center.service";
 
 @Component({
   selector: 'app-center-details',
   templateUrl: './center-details.component.html',
   styleUrls: ['./center-details.component.css']
 })
-export class CenterDetailsComponent {
+export class CenterDetailsComponent implements OnInit {
+  centerDetailsState$: Observable<AppState<CustomHttpResponse<CenterDetailsResponse>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<CenterDetailsResponse>>(null);
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingSubject.asObservable();
 
+  readonly DataState = DataState;
+
+  private readonly CENTER_ID: string = 'id';
+
+  constructor(private activatedRoute: ActivatedRoute, private centerService: CenterService) {
+  }
+
+  ngOnInit(): void {
+    this.centerDetailsState$ = this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        return this.centerService.centerDetails$(+params.get(this.CENTER_ID))
+          .pipe(
+            map(response => {
+              console.log(response);
+              this.dataSubject.next(response);
+              return { dataState: DataState.LOADED, appData: response };
+            }),
+
+            startWith({ dataState: DataState.LOADING }),
+
+            catchError((error: string) => {
+              return of({ dataState: DataState.ERROR, error })
+            })
+          )
+      })
+    );
+  }
 }
