0d40d865705d4d25ebb3e7658edc62adeaf83410
Andrei-David Nan <nandavid190@gmail.com>
2024-01-02 12:04:28 +0200

    v0.5.8 - stats component responsiveness, styling, consistency improvements, set up groundwork for changes to come;

--- src/app/component/stats/stats.component.ts
+++ src/app/component/stats/stats.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css']
 })
-export class StatsComponent {
+export class StatsComponent implements OnInit {
+
+  @Input() rewardPoints: number;
+  @Input() activeRecyclers: number;
+  @Input() recyclersRewardPoints: number
+
+  recyclersEnvironmentalImpact: number;
+  userEnvironmentalImpact: number;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
