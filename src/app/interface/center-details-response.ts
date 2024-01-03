aeae36ddb5ba10c1d400cc3056ee21d584880231
Andrei-David Nan <nandavid190@gmail.com>
2024-01-03 14:58:41 +0200

    v0.5.17 - add center stats interface;

--- src/app/interface/center-details-response.ts
+++ src/app/interface/center-details-response.ts
@@ -1,10 +1,12 @@
 import {RecyclingCenter} from "./recycling-center";
 import {User} from "./user";
 import {RecyclingActivity} from "./recycling-activity";
+import {CenterStats} from "./center-stats";
 
 export interface CenterDetailsResponse {
   user: User;
   center: RecyclingCenter;
   activities: RecyclingActivity[];
   rewardPoints: Number;
+  centerStats: CenterStats
 }
---
+++ src/app/interface/center-stats.ts
@@ -0,0 +1,10 @@
+export interface CenterStats {
+  recyclersNumber?: number;
+  activitiesNumber?: number;
+  plasticRecycled?: number;
+  paperRecycled?: number;
+  glassRecycled?: number;
+  aluminumRecycled?: number;
+  electronicsRecycled?: number;
+  metalsRecycled?: number;
+}
