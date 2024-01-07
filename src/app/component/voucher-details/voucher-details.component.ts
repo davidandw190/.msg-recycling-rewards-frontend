b4ff730130b118c44a1d32963d937cc1710f181b
Andrei-David Nan <nandavid190@gmail.com>
2024-01-07 21:28:46 +0200

    v0.6.12 - configure voucher details component logic, add msg-logo.svg;

--- src/app/component/voucher-details/voucher-details.component.ts
+++ src/app/component/voucher-details/voucher-details.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, catchError, map, Observable, of, startWith, switchMap} from "rxjs";
+import {CustomHttpResponse} from "../../interface/custom-http-response";
+import {VoucherDetailsResponse} from "../../interface/voucher-details-response";
+import { AppState } from 'src/app/interface/app-state';
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import { DataState } from 'src/app/enum/data-state.enum';
+import {VoucherService} from "../../service/voucher.service";
+
+const VOUCHER_CODE: string = 'code';
 
 @Component({
   selector: 'app-voucher-details',
   templateUrl: './voucher-details.component.html',
   styleUrls: ['./voucher-details.component.css']
 })
-export class VoucherDetailsComponent {
+export class VoucherDetailsComponent implements OnInit {
+  voucherDetailsState$: Observable<AppState<CustomHttpResponse<VoucherDetailsResponse>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<VoucherDetailsResponse>>(null);
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingSubject.asObservable();
+  readonly DataState = DataState;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private voucherService: VoucherService
+  ) { }
+
+  ngOnInit(): void {
+    this.voucherDetailsState$ = this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        return this.voucherService.voucher$(params.get(VOUCHER_CODE))
+          .pipe(
+            map(response => {
+              console.log(response);
+              this.dataSubject.next(response);
+              return { dataState: DataState.LOADED, appData: response };
+            }),
+
+            startWith({ dataState: DataState.LOADING }),
+
+            catchError((error: string) => {
+              return of({ dataState: DataState.ERROR, error })
+            })
+          )
+      })
+    );
+  }
 }
