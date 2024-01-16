07f958b3de430134d7499562296b763e2076d801
Andrei-David Nan <nandavid190@gmail.com>
2024-01-16 23:47:19 +0200

    v0.7.19 - fix issue regarding navbar submenus no longer triggering;

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -11,13 +11,44 @@ import {UserService} from "../../service/user.service";
 export class NavbarComponent {
   @Input() user: User;
 
+  isNavbarCollapsed = true;
+  isSmallScreen = false;
+
+
+
+
   constructor(
     private router: Router,
     private userService: UserService
-  ) {}
+  ) {
+  }
+
+  isNavbarOpen = false;
+
+  centerLinks = [
+    { label: 'All Centers', route: '/centers/all' },
+    { label: 'My Favourites', route: '/centers/favourites' },
+  ];
+
+  challengeLinks = [
+    { label: 'All Challenges', route: '/challenges/all' },
+    { label: 'My Challenges', route: '/challenges/my' },
+  ];
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
 
   logOut(): void {
     this.userService.logOut();
     this.router.navigate(['/login'])
   }
+
+  private checkScreenSize() {
+    this.isSmallScreen = window.innerWidth < 768;
+  }
+
+  // toggleNavbar() {
+  //   this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  // }
 }
